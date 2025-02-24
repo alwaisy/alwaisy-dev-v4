5af21d766de11de8bc6866c978c6d482fe877635
Awais alwaisy <git@alwaisy.dev>
2025-02-24 19:56:13 +0500

    just wasted whole day

---
+++ notes.md
@@ -0,0 +1,16 @@
+-Dark1: #141414
+-Dark2: #1A1A1A
+-Dark3: #1F1F1F
+-Dark4: #2B2B2B
+-Dark5: #000000 / 50%
+-Dark8: #000000 / 8%
+
+-White100: #FFFFFF /100
+-White90: #FFFFFF / 90%
+-White80: #FFFFFF /80%
+-White70: #FFFFFF /70%
+-White60: #FFFFFF / 60%
+-White50: #FFFFFF / 50%
+-White40: #FFFFFF / 40%
+
+-Border8: #FFFFFF / 8%
--- src/components/home/h-hero/h-hero.tsx
+++ src/components/home/h-hero/h-hero.tsx
@@ -3,24 +3,30 @@
 import { ShootingStars } from "~/components/aceternityui/shooting-stars";
 import { StarsBackground } from "~/components/aceternityui/stars-background";
 import { Meteors } from "~/components/magicui/meteors";
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import Container from "~/components/ui/container";
 
 const HomeHero = () => {
   return (
-    <Container
-      as="section"
-      className="relative flex h-[40rem] flex-col items-center overflow-hidden rounded-md bg-neutral-900"
-    >
-      <h2 className="relative z-10 mx-auto flex max-w-5xl flex-col items-center gap-2 bg-gradient-to-b from-neutral-800 via-white to-white bg-clip-text text-center text-3xl font-medium tracking-tight text-transparent md:flex-row md:gap-8 md:text-5xl md:leading-tight">
-        <span>Shooting Star</span>
-        <span className="text-lg font-thin text-white">x</span>
-        <span>Star Background</span>
-      </h2>
-      <ShootingStars />
-      <StarsBackground />
-      <Meteors />
+    <Container as="section" className="border-b border-border/10">
+      <div className="relative mx-auto h-[595px] max-w-[740px] overflow-hidden border-x border-dashed border-border/10 px-12 py-20 pb-10">
+        <button className="btn relative z-10">Contact</button>
 
-      <button className="btn z-10">Contact</button>
+        <div className="flex h-min content-center items-center justify-start gap-2 overflow-visible rounded-none p-0">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <p className="text-accent-foreground">
+            Doubled growth in 6 months with Andrew.
+          </p>
+        </div>
+
+        {/* background */}
+        <ShootingStars />
+        <StarsBackground />
+        <Meteors />
+      </div>
     </Container>
   );
 };
